caa54a262a489c225ded1149b93d51bb98610edc
Cory Forsstrom <cforsstrom18@gmail.com>
2022-12-03 09:36:56 -0800

    init

---
+++ 2022/app/Main.hs
@@ -0,0 +1,6 @@
+module Main (main) where
+
+import qualified AdventOfCode
+
+main :: IO ()
+main = AdventOfCode.run
---
+++ 2022/src/AdventOfCode.hs
@@ -0,0 +1,27 @@
+module AdventOfCode
+    ( run 
+    ) where
+
+run :: IO ()
+run = do
+    inputs <- mapM getInput days
+    _ <- mapM (uncurry displayDay) $ zip days inputs
+    return ()
+
+data Day = Day { _number :: Int, _run :: String -> String }
+
+newDay :: Int -> (String -> String) -> Day
+newDay _number _run = Day _number _run
+
+days :: [Day]
+days = map (uncurry newDay) $ zip [1..] []
+
+displayDay :: Day -> String -> IO ()
+displayDay (Day number _run) input = do
+    putStrLn $ "--- Day " ++ show number ++ " ---"
+    putStrLn result
+    putStrLn ""
+    where result = _run input
+    
+getInput :: Day -> IO String
+getInput (Day number _) = readFile $ "input/Day" ++ show number ++ ".txt"
